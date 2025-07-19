d9564ef432b411b635e90c9fa39117d2257af196
jiangrui-zheng <zjr769806@gmail.com>
2025-07-19 03:53:48 -0400

    update 3.6.13

--- lib/address-list/AddressListItem.d.ts
+++ lib/address-list/AddressListItem.d.ts
@@ -1,34 +1,46 @@
 import { type PropType } from 'vue';
 import { type Numeric } from '../utils';
-export declare type AddressListAddress = {
+export type AddressListAddress = {
     id: Numeric;
     tel: Numeric;
     name: string;
     address: string;
     isDefault?: boolean;
 };
-declare const _default: import("vue").DefineComponent<{
+declare const _default: import("vue").DefineComponent<import("vue").ExtractPropTypes<{
     address: {
         type: PropType<AddressListAddress>;
         required: true;
     };
     disabled: BooleanConstructor;
     switchable: BooleanConstructor;
+    singleChoice: BooleanConstructor;
     defaultTagText: StringConstructor;
-}, () => JSX.Element, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, ("select" | "click" | "edit")[], "select" | "click" | "edit", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    rightIcon: {
+        type: PropType<T>;
+        default: string;
+    };
+}>, () => import("vue/jsx-runtime").JSX.Element, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, ("select" | "click" | "edit")[], "select" | "click" | "edit", import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{
     address: {
         type: PropType<AddressListAddress>;
         required: true;
     };
     disabled: BooleanConstructor;
     switchable: BooleanConstructor;
+    singleChoice: BooleanConstructor;
     defaultTagText: StringConstructor;
-}>> & {
+    rightIcon: {
+        type: PropType<T>;
+        default: string;
+    };
+}>> & Readonly<{
     onClick?: ((...args: any[]) => any) | undefined;
     onSelect?: ((...args: any[]) => any) | undefined;
     onEdit?: ((...args: any[]) => any) | undefined;
-}, {
+}>, {
     disabled: boolean;
+    rightIcon: string;
     switchable: boolean;
-}>;
+    singleChoice: boolean;
+}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
 export default _default;
